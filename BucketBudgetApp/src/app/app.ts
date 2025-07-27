cec902f26616e2ffca2b79c4ef5ccc4255eee9cf
Ben Murray <beckmurray94@gmail>
2025-07-27 17:54:56 -0400

    Adding Transactions component

--- BucketBudgetApp/src/app/app.ts
+++ BucketBudgetApp/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BucketSummary } from './bucket-summary/bucket-summary';
+import { Transactions } from './transactions/transactions';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
     BucketSummary,
+    Transactions
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
